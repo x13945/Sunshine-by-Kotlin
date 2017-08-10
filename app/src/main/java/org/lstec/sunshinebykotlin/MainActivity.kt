42bcf2dc5308589462ce84badc51e18c7f07cba5
shaw <x13945@163.com>
2017-08-10 20:05:03 +0800

    feature: add test code to verify the project

--- app/src/main/java/org/lstec/sunshinebykotlin/MainActivity.kt
+++ app/src/main/java/org/lstec/sunshinebykotlin/MainActivity.kt
@@ -3,10 +3,13 @@ package org.lstec.sunshinebykotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        message.text = "Hello world from kotlin!"
     }
 }
