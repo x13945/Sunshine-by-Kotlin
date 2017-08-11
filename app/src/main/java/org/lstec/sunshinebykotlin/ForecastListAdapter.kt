eb98c7e52dfc1ee5de617312d64fe2a9f3005254
shaw <x13945@163.com>
2017-08-11 12:14:30 +0800

    feature: add test view to main activity

---
+++ app/src/main/java/org/lstec/sunshinebykotlin/ForecastListAdapter.kt
@@ -0,0 +1,24 @@
+package org.lstec.sunshinebykotlin
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * Created by shaw on 10/08/2017.
+ */
+class ForecastListAdapter(val items: List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        return ViewHolder(TextView(parent?.context))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        holder?.textView?.text = items[position]
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class ViewHolder(val textView:TextView) : RecyclerView.ViewHolder(textView)
+}
