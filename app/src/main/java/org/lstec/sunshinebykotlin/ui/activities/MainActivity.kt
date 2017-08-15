8b46b5a6b3240bb8212eebbc129d379404da8e81
shaw <x13945@163.com>
2017-08-15 17:35:49 +0800

    feature: Kotlin Android Extensions

--- app/src/main/java/org/lstec/sunshinebykotlin/ui/activities/MainActivity.kt
+++ app/src/main/java/org/lstec/sunshinebykotlin/ui/activities/MainActivity.kt
@@ -3,7 +3,7 @@ package org.lstec.sunshinebykotlin.ui.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
@@ -16,7 +16,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
         forecastList.layoutManager = LinearLayoutManager(this)
 
         doAsync {
--- app/src/main/java/org/lstec/sunshinebykotlin/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/org/lstec/sunshinebykotlin/ui/adapters/ForecastListAdapter.kt
@@ -4,14 +4,12 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.squareup.picasso.Picasso
 import org.lstec.sunshinebykotlin.R
 import org.lstec.sunshinebykotlin.domain.model.Forecast
 import org.lstec.sunshinebykotlin.domain.model.ForecastList
 import org.lstec.sunshinebykotlin.ui.utils.ctx
-import org.jetbrains.anko.find
+import kotlinx.android.synthetic.main.item_forecast.view.*
 
 /**
  * Created by shaw on 10/08/2017.
@@ -31,21 +29,16 @@ class ForecastListAdapter(val weekForecast: ForecastList,
 
     class ViewHolder(view: View,
                      val itemClick: (Forecast) -> Unit) : RecyclerView.ViewHolder(view){
-        private val iconView = view.find<ImageView>(R.id.icon)
-        private val dateView = view.find<TextView>(R.id.date)
-        private val descriptionView = view.find<TextView>(R.id.description)
-        private val maxTemperature = view.find<TextView>(R.id.maxTemperature)
-        private val minTemperature = view.find<TextView>(R.id.minTemperature)
 
         fun bindForecast(forecast: Forecast){
             with(forecast){
                 Picasso.with(itemView.ctx)
                         .load(iconUrl)
-                        .into(iconView)
-                dateView.text = date
-                descriptionView.text = description
-                maxTemperature.text = "${high}º"
-                minTemperature.text = "${low}º"
+                        .into(itemView.icon)
+                itemView.date.text = date
+                itemView.description.text = description
+                itemView.maxTemperature.text = "${high}º"
+                itemView.minTemperature.text = "${low}º"
                 itemView.setOnClickListener { itemClick(this) }
             }
         }
